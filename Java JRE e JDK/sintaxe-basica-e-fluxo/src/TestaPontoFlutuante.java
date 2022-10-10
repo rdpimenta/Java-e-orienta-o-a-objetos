1d69c6dbb61db8659f24a456261e54bb5a87b891
Roger Pimenta <roger.pimenta@live.com>
2022-10-10 17:51:53 -0300

    variaveis

---
+++ Java JRE e JDK/sintaxe-basica-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,19 @@
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+        double salario;
+        salario = 1250.70;
+        System.out.println("meu salário é " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        int divisaoInt = 5 / 2;
+        System.out.println(divisaoInt);
+
+        double divisaoDouble = 5.0 / 2;
+        System.out.println(divisaoDouble);
+
+        double salarioInicializado = 1250.70;
+        System.out.println(salarioInicializado);
+    }
+}
