e0c4049c467e92122a8570b3aaed1cb1b67ab806
Roger Pimenta <roger.pimenta@live.com>
2022-10-13 10:36:59 -0300

    composição de objetos

--- Entendendo a orientação a objetos/bytebank/src/TestaMetodo.java
+++ Entendendo a orientação a objetos/bytebank/src/TestaMetodo.java
@@ -21,5 +21,8 @@ public class TestaMetodo {
 
         System.out.println(contaDoPaulo.saldo);
         System.out.println(contaDaMarcela.saldo);
+
+        contaDoPaulo.titular = "paulo silveira";
+        System.out.println(contaDoPaulo.titular);
     }
 }
