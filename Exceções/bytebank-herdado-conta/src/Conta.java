ef4e7a22e4b9be193bb5a3f183bced27ca1a48d1
Roger Pimenta <roger.pimenta@live.com>
2022-10-20 16:48:55 -0300

    aplicando exceções

---
+++ Exceções/bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,67 @@
+public abstract class Conta {
+    protected double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public abstract void deposita(double valor);
+
+    public void saca(double valor) {
+        if (this.saldo <= valor) {
+            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+        }
+        this.saldo -= valor;
+    }
+
+    public void transfere(double valor, Conta destino) {
+        this.saca(valor);
+        destino.deposita(valor);
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        if (numero <= 0) {
+            System.out.println("não pode valor <= 0");
+            return;
+        }
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("nao pode valor menor igual a 0");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
+    }
+}
