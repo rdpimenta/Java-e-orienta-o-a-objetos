8d8f6147370f3ca4fcb21a6d3906c9918e872a09
Roger Pimenta <roger.pimenta@live.com>
2022-10-21 16:54:58 -0300

    guradando qualquer referência

--- Coleções, wrappers e lambda expressions/listas/src/TesteArrayDeReferencias.java
+++ Coleções, wrappers e lambda expressions/listas/src/TesteArrayDeReferencias.java
@@ -1,14 +1,18 @@
 public class TesteArrayDeReferencias {
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+        Conta[] contas = new Conta[5];
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
         contas[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
         contas[1] = cc2;
 
         System.out.println(cc2.getNumero());
         System.out.println(contas[1].getNumero());
+
+        ContaPoupanca ref = (ContaPoupanca) contas[1];
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
     }
 }
---
+++ Coleções, wrappers e lambda expressions/listas/src/TesteArrayString.java
@@ -0,0 +1,9 @@
+public class TesteArrayString {
+    public static void main(String[] args) {
+        System.out.println("Funcionou!!");
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
