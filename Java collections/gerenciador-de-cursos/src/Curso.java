0c7116a96b4560f1f1b33e9ec64c9d8945493370
Roger Pimenta <roger.pimenta@live.com>
2022-10-31 17:13:42 -0300

    mais práticas de relacionamentos

--- Java collections/gerenciador-de-cursos/src/Curso.java
+++ Java collections/gerenciador-de-cursos/src/Curso.java
@@ -27,4 +27,14 @@ public class Curso {
     public void adiciona(Aula aula) {
         this.aulas.add(aula);
     }
+
+    public int getTempoTotal() {
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal()
+                + ", aulas: " + this.aulas + "]";
+    }
 }
