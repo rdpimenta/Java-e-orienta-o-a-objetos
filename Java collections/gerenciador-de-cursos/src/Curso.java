bf4e9d6ed041369b795debd44ef11fd5195dd435
Roger Pimenta <roger.pimenta@live.com>
2022-11-01 16:28:38 -0300

    aplicando o set no modelo

--- Java collections/gerenciador-de-cursos/src/Curso.java
+++ Java collections/gerenciador-de-cursos/src/Curso.java
@@ -1,11 +1,10 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -24,6 +23,10 @@ public class Curso {
         return Collections.unmodifiableList(aulas);
     }
 
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
+
     public void adiciona(Aula aula) {
         this.aulas.add(aula);
     }
@@ -36,5 +39,9 @@ public class Curso {
     public String toString() {
         return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal()
                 + ", aulas: " + this.aulas + "]";
+    }
+
+    public void matricula(Aluno aluno) {
+        this.alunos.add(aluno);
     }
 }
