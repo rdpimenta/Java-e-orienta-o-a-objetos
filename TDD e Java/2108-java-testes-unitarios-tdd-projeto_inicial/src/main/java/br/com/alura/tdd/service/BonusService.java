5daa9ab5c97a41f70614b6477c01b5c2d77a3125
Roger Pimenta <roger.pimenta@live.com>
2022-11-04 16:21:43 -0300

    Lidando com exceptions

--- TDD e Java/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ TDD e Java/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -10,7 +10,7 @@ public class BonusService {
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionários com salário acima de 10000 não recebem bônus.");
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
--- TDD e Java/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ TDD e Java/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -2,6 +2,9 @@ package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Funcionario;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -11,9 +14,13 @@ public class BonusServiceTest {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService bonusService = new BonusService();
-        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")))
+        );
+
+        assertEquals("Funcionários com salário acima de 10000 não recebem bônus.", exception.getMessage());
     }
 
     @Test
